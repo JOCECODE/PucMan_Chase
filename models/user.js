564bdacea944f4908e56d258e9de99f8114feb6b
Stuart Wong <swong31@gmail.com>
2020-10-07 19:14:53 -0700

    ADD FRONTEND JS TO GAMESTART.HTML

--- models/user.js
+++ models/user.js
@@ -9,14 +9,14 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true,
-      },
+        isEmail: true
+      }
     },
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
-    },
+      allowNull: false
+    }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
