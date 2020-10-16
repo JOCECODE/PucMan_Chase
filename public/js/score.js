cfb7aacb58d7c9823cfac8be08eb75c99f2d5b71
JOCECODE <ulah57@gmail.com>
2020-10-15 19:17:18 -0700

    ADD END SONG

--- public/js/score.js
+++ public/js/score.js
@@ -1,4 +1,8 @@
+const endMusic = document.querySelector("#dogLaugh")
+
 $(document).ready(function() {
   $("#scoreTransition").hide();
   $("#scoreTransition").fadeIn(400);
+  endMusic.play();
+
 });
