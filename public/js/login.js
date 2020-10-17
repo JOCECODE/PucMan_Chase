89b89355f5e097a221b3d7337b2ccf6f4c0b3fc2
Melinda Lindsey <mperales13@gmail.com>
2020-10-17 00:35:57 -0700

    doing a general save

--- public/js/login.js
+++ public/js/login.js
@@ -6,109 +6,20 @@ $(document).ready(() => {
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
 
-  function play() {
-    let audio = document.getElementById("audio");
-    return audio.play();
+
+  //
+  // function redirect(){
+  //   document.location.href="../public/gamestart.html";
+  //   return redirect();
+  // }
+
+  function newLocation() {
+    document.location.href="gamestart.html";
   }
 
-  // window.onload = function() {
-  //   document.getElementById("#pop").play();
-  // };
-  //
-  // $(document).ready(() => {
-  //   const keys = {
-  //     13: "Enter",
-  //     37: "ArrowLeft",
-  //     38: "ArrowUp",
-  //     39: "ArrowRight",
-  //     40: "ArrowDown",
-  //     65: "a",
-  //     66: "b"
-  //   };
-  //
-  //   const konamiCode = [
-  //     "ArrowUp",
-  //     "ArrowUp",
-  //     "ArrowDown",
-  //     "ArrowDown",
-  //     "ArrowLeft",
-  //     "ArrowRight",
-  //     "ArrowLeft",
-  //     "ArrowRight",
-  //     "b",
-  //     "a",
-  //     "Enter"
-  //   ];
-  //   let userInput = 0;
-  //   $("backgroundImage").html("insert image here");
-  //   document.addEventListener("keydown", e => {
-  //     // starts 2nd code
-  //
-  //     const key = keys[e.keyCode];
-  //     const requiredKey = konamiCode[userInput];
-  //
-  //     if (key === requiredKey) {
-  //       userInput++;
-  //       if (userInput === konamiCode.length) {
-  //         activateCheats();
-  //         userInput = 0;
-  //       }
-  //     } else {
-  //       userInput = 0;
-  //     }
-  //   });
-  //   function activateCheats() {
-  //     $("pop").play();
-  //     $("h1").html("Code Accepted");
-  //   }
-  // });
-
-  window.onload=function() {
-    document.getElementById("pop").play();
-  };
-
-  $(document).ready(function(){
 
 
-    var keys = {
-      13: 'Enter',
-      37: 'ArrowLeft',
-      38: 'ArrowUp',
-      39: 'ArrowRight',
-      40: 'ArrowDown',
-      65: 'a',
-      66: 'b'
-    };
 
-    var konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
-    var userInput = 0;
-
-    $('backgroundImage').html('');
-    document.addEventListener('keydown', function (e) { // starts 2nd code
-
-      var key = keys[e.keyCode];
-      var requiredKey = konamiCode[userInput];
-
-      if (key === requiredKey) {
-        userInput++;
-        if (userInput === konamiCode.length) {
-          activateCheats();
-          userInput = 0;
-        }
-      } else {
-        userInput = 0;
-      }
-    });
-    function activateCheats() {
-      // $('body').css();
-
-      $('h1').html("You Have 30 Extra Lives");
-
-      // $('body').css('background-size', '100px 100px');
-
-
-    }
-  });
 
   // When the form is submitted, we validate there's an email and password entered
   loginForm.on("submit", event => {
--- public/login-page.js
+++ public/login-page.js
@@ -1,9 +1,127 @@
-"use strict"
+"use strict";
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
+function play() {
+  const audio = document.getElementById("audio");
+  return audio.play();
+}
+
+function newLocation() {
+  document.location.href = "gamestart.html";
+}
+
+// window.onload = function() {
+//   document.getElementById("#pop").play();
+// };
+//
+// $(document).ready(() => {
+//   const keys = {
+//     13: "Enter",
+//     37: "ArrowLeft",
+//     38: "ArrowUp",
+//     39: "ArrowRight",
+//     40: "ArrowDown",
+//     65: "a",
+//     66: "b"
+//   };
+//
+//   const konamiCode = [
+//     "ArrowUp",
+//     "ArrowUp",
+//     "ArrowDown",
+//     "ArrowDown",
+//     "ArrowLeft",
+//     "ArrowRight",
+//     "ArrowLeft",
+//     "ArrowRight",
+//     "b",
+//     "a",
+//     "Enter"
+//   ];
+//   let userInput = 0;
+//   $("backgroundImage").html("insert image here");
+//   document.addEventListener("keydown", e => {
+//     // starts 2nd code
+//
+//     const key = keys[e.keyCode];
+//     const requiredKey = konamiCode[userInput];
+//
+//     if (key === requiredKey) {
+//       userInput++;
+//       if (userInput === konamiCode.length) {
+//         activateCheats();
+//         userInput = 0;
+//       }
+//     } else {
+//       userInput = 0;
+//     }
+//   });
+//   function activateCheats() {
+//     $("pop").play();
+//     $("h1").html("Code Accepted");
+//   }
+// });
+document.getElementById("pac-man-intro.mp3").src = "movie.mp4";
+// window.onload = function pop() {
+//   document.getElementById("#pop").play();
+//   return pop();
+// };
+
+$(document).ready(() => {
+  const keys = {
+    13: "Enter",
+    37: "ArrowLeft",
+    38: "ArrowUp",
+    39: "ArrowRight",
+    40: "ArrowDown",
+    65: "a",
+    66: "b"
+  };
+
+  const konamiCode = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a",
+    "Enter"
+  ];
+  let userInput = 0;
+
+  $("backgroundImage").html("");
+  document.addEventListener("keydown", e => {
+    // starts 2nd code
+
+    const key = keys[e.keyCode];
+    const requiredKey = konamiCode[userInput];
+
+    if (key === requiredKey) {
+      userInput++;
+      if (userInput === konamiCode.length) {
+        activateCheats();
+        userInput = 0;
+      }
+    } else {
+      userInput = 0;
+    }
+  });
+  function activateCheats() {
+    // $('body').css();
+
+    $("h1").html("You Have 30 Extra Lives");
+
+    // $('body').css('background-size', '100px 100px');
+  }
+});
+
+loginButton.addEventListener("click", e => {
   e.preventDefault();
   const username = loginForm.username.value;
   const password = loginForm.password.value;
@@ -14,4 +132,4 @@ loginButton.addEventListener("click", (e) => {
   } else {
     loginErrorMsg.style.opacity = 1;
   }
-})
+});
